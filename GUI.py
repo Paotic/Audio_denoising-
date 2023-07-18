4a905d3ff791335a3800526392a6916ff5b0818f
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-07-18 23:13:22 +0700

    Fix the issuse

--- GUI.py
+++ GUI.py
@@ -7,24 +7,21 @@ window.title("Audio_denoising")
 window.geometry("1280x1080")
 window.resizable(False,False)
 pygame.mixer.init()
-# func play,freq_denoise,moving_denoise,spectral_denoise use to play the record
+# func play,freq_denoise,spectral_denoise use to play the record
 def play():
     pygame.mixer.music.load("test_noise.wav")
     pygame.mixer.music.play(loops= 0)
 def freq_denoise():
     pygame.mixer.music.load("freq_flitering.wav")
     pygame.mixer.music.play(loops= 0)
-def moving_denoise():
-    pygame.mixer.music.load("moving_avg_fliter.wav")
-    pygame.mixer.music.play(loops= 0)
+
 def spectral_denoise():
     pygame.mixer.music.load("spectral_sub.wav")
     pygame.mixer.music.play(loops= 0)
 # import the 3 files
 def freq():
     import freq_filter
-def moving():
-    import moving_average
+
     
 def spectral():
     import spectral_subtraction
@@ -44,20 +41,17 @@ myLabel2 = Label(window, text="Option: ",font= 25,bg="#fff") # A label for 3 opt
 myLabel2.place(relx= 0.05,rely= 0.30)
 
 freq_btn = Button(window, text="Freq filter",font= 20,bg="#fff",activebackground="#313131",command=freq) # A button to show the diagram of freq
-freq_btn.place(relx= 0.1, rely= 0.35, relheight= 0.05,relwidth=0.2)
+freq_btn.place(relx= 0.2, rely= 0.35, relheight= 0.05,relwidth=0.2)
 
-moving_btn = Button(window, text="Moving average",font= 20,bg="#fff",activebackground="#313131",command= moving) # A button to show the diagram of avarage
-moving_btn.place(relx= 0.4, rely= 0.35, relheight= 0.05,relwidth=0.2)
 
 spectral_btn = Button(window, text="Spectral subtraction",font= 20,bg="#fff",activebackground="#313131",command= spectral) # A button to show the diagram of subtraction
-spectral_btn.place(relx= 0.7, rely= 0.35, relheight= 0.05,relwidth=0.2)
+spectral_btn.place(relx= 0.5, rely= 0.35, relheight= 0.05,relwidth=0.2)
 
 
 freq_button = Button(window, text="Freq filter sound", font= 20,bg="#fff",activebackground="#313131",command=freq_denoise) # A button to show the diagram of freq sound
-freq_button.place(relx= 0.1, rely= 0.45, relheight= 0.05,relwidth=0.2)
-moving_button = Button(window, text="Moving avarage sound", font= 20,bg="#fff",activebackground="#313131",command=moving_denoise) # A button to show the diagram of avarage sound
-moving_button.place(relx= 0.4, rely= 0.45, relheight= 0.05,relwidth=0.2)
+freq_button.place(relx= 0.2, rely= 0.45, relheight= 0.05,relwidth=0.2)
+
 spectral_button = Button(window, text="Specreal subtraction sound", font= 20,bg="#fff",activebackground="#313131",command=spectral_denoise) # A button to show the diagram of subtraction sound
-spectral_button.place(relx= 0.7, rely= 0.45, relheight= 0.05,relwidth=0.2)
+spectral_button.place(relx= 0.5, rely= 0.45, relheight= 0.05,relwidth=0.2)
 
 window.mainloop()
--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 ## Audio Denoise
 
-We are using 3 algorithms to perform noise filtering on test_noise.wav audio file which contains some background noise.
+We are using 2 algorithms to perform noise filtering on test_noise.wav audio file which contains some background noise.
 
 The algorithms used are:-
 1) Time Domain - Moving Average Filter (moving_average.py)
-2) Frequency Domain Filter (freq_filter.py)
-3) Spectral Subtraction (spectral_subtraction.py)
+2) Spectral Subtraction (spectral_subtraction.py)
